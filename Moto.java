16f299013b3afca320c3ee64de28d075250605e2
J-Glzz <jgonzalezc1704@alumnp.ipn.mx>
2022-04-13 12:29:07 -0500

    Agregue otro texto

--- Moto.java
+++ Moto.java
@@ -8,7 +8,7 @@ que el programa tengo un bajo nivel de acoplamiento y un alto nivel
 de cohesión. Entonces para que quede más claro, debe relacionarse poco
 (bajo acoplamiento) y hacer solo una cosa de manera correcta (alta cohesión).
 Con definir el constructor, ya no se tienen que poner los Setters, porque
-como lo mencionó anteriormente, ya se define eso con el constructor.
+como lo mencionó anteriormente, ya se define eso con el constructor..
 */
 public class Moto {
     private int numeroRuedas;
